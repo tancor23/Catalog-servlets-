f90b8b52168ad84fe7836612a2858b4f57bff30e
Sergei Vydra <sergei.vydra@itrexgroup.com>
2019-10-30 00:23:03 +0300

    Switched xml to annotation

--- src/main/java/com/itrexgroup/vydrasergei/bookcatalog/MainServlet.java
+++ src/main/java/com/itrexgroup/vydrasergei/bookcatalog/MainServlet.java
@@ -1,15 +1,18 @@
 package com.itrexgroup.vydrasergei.bookcatalog;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/")
 public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("name", "ITREx Group");
         req.getRequestDispatcher("main_page.jsp").forward(req, resp);
     }
 
